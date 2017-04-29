729602b975f58bb2afe8da934cbdb44bdc1d4193
Alex <luchcko.a@gmail.com>
2017-04-29 05:29:35 +0300

    finally unit tests for counter reducer

--- src/app/reducers/counter.spec.ts
+++ src/app/reducers/counter.spec.ts
@@ -2,34 +2,24 @@ import * as fromCounter from './counter';
 import {IncrementCounter, DecrementCounter} from '../actions/counter';
 
 describe('CounterReducer', () => {
-  function fiveIterator(state: {counter: number}, action, iterator?: number) {
-    for (let i = 0; i < iterator || 5; i++) {
-      if (i === 4) {
-        return fromCounter.reducer(state, action);
-      }
-      fromCounter.reducer(state, action);
-    }
-  }
-
-  describe('Increment', () => {
-    it('should return the initial state', () => {
-      const result = {counter: 0};
-      expect(result).toEqual(fromCounter.initialState);
-    });
-
-    it('should return counter = 1', () => {
-      const state = {counter: 0}
-          , expectedOne = {counter: 1}
-          , expectedFive = {counter: 5}
-          , action = new IncrementCounter()
-          , actualOneIncrement = fromCounter.reducer(state, action);
-
-      expect(actualOneIncrement).toEqual(expectedOne);
-      // expect(fiveIterator(state, action)).toEqual(expectedFive);
-    });
+  it('should return the initial state', () => {
+    const result = {counter: 0};
+    expect(result).toEqual(fromCounter.initialState);
   });
 
-  describe('Decrement', () => {
+  it('should return increment value = 1', () => {
+    const expected = {counter: 1}
+        , action = new IncrementCounter()
+        , actual = fromCounter.reducer(fromCounter.initialState, action);
 
+    expect(actual).toEqual(expected);
+  });
+
+  it('should return decrement value = -1', () => {
+    const expected = {counter: -1}
+      , action = new DecrementCounter()
+      , actual = fromCounter.reducer(fromCounter.initialState, action);
+
+    expect(actual).toEqual(expected);
   });
 });
